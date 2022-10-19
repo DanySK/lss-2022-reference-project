bb150e3205144d4086a47e3ab6f600b05146c20e
Danilo Pianini <danilo.pianini@unibo.it>
2022-10-19 16:10:52 +0200

    test: add minimal testing for the plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    `java-gradle-plugin` // Injects the plugin classpath into the tests
     kotlin("jvm") version "1.7.20"
 }
 
@@ -9,4 +10,11 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(gradleApi())
+    implementation(gradleKotlinDsl())
+    testImplementation(gradleTestKit())
+    testImplementation(libs.bundles.kotlin.testing)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
