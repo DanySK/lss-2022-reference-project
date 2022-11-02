f18a9ce023c5641f8194865c811be7ec98794b9e
Danilo Pianini <danilo.pianini@unibo.it>
2022-11-02 14:09:08 +0100

    build: enable git-sensitive versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
+import org.gradle.kotlin.dsl.*
+
 plugins {
     `java-gradle-plugin` // Injects the plugin classpath into the tests
     kotlin("jvm") version "1.7.20"
+    id("org.danilopianini.git-sensitive-semantic-versioning-gradle-plugin") version "0.3.20"
 }
+
+version = "0.1.0"
 
 repositories {
     mavenCentral()
