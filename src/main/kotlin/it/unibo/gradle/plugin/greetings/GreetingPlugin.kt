5fdd5864325b49c160ddcef413aebc4600c20b06
Danilo Pianini <danilo.pianini@unibo.it>
2022-11-09 13:17:20 +0100

    style: solve the Detekt issues

--- src/main/kotlin/it/unibo/gradle/plugin/greetings/GreetingPlugin.kt
+++ src/main/kotlin/it/unibo/gradle/plugin/greetings/GreetingPlugin.kt
@@ -11,6 +11,9 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.property
 import org.gradle.kotlin.dsl.register
 
+/**
+ * Sample plugin.
+ */
 open class GreetingPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
@@ -22,27 +25,45 @@ open class GreetingPlugin : Plugin<Project> {
     }
 }
 
+/**
+ * Sample [project] extension.
+ */
 open class GreetingsExtension(project: Project) {
 
     internal val target: Property<String> = project.objects.property<String>()
         .apply { convention("everybody") }
 
+    /**
+     * Uses the string as a greeting.
+     */
     fun String.asGreeting() = target.set(this)
 }
 
-open class GreetingTask() : DefaultTask() {
+/**
+ * Greeting task.
+ */
+open class GreetingTask : DefaultTask() {
 
     init {
         group = "Custom tasks"
         description = "a task that just prints"
     }
 
+    /**
+     * The greeting.
+     */
     @Input
     val greeting: Property<String> = project.objects.property()
 
+    /**
+     * The greeting message.
+     */
     @Internal
     val message: Provider<String> = greeting.map { "I salute you with: $it" }
 
+    /**
+     * Actual task action. Prints a greeting.
+     */
     @TaskAction
     fun printGreeting() {
         project.logger.warn(message.get())
